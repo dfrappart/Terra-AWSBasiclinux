badbc1d5a2d0f3607b5bbfb9e74bb3583c84e189
David Frappart <david@frappart.com>
2017-11-04 13:37:03 +0100

    minor modif

--- template.tf
+++ template.tf
@@ -1247,3 +1247,20 @@ output "Private IP  DB2" {
 output "FQDN du Web Load Balancer" {
   value = "${aws_alb.BasicLinux-WebALB.dns_name}"
 }
+
+
+/*
+Copyright 2017 David Frappart
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
